74319381da84ede94bf95e2ced22f80d27cd93f0
Aryn02 <98638410+Aryn02@users.noreply.github.com>
2023-02-12 17:23:34 +0600

    cmt

--- lab3/task2/todo.js
+++ lab3/task2/todo.js
@@ -15,6 +15,7 @@ function addTask() {
         const task_el = document.createElement("div");
         task_el.classList.add("task");
 
+        //checkbox
         const task_checkdiv_el = document.createElement("div");
         task_checkdiv_el.classList.add("check");
 
@@ -25,13 +26,26 @@ function addTask() {
         task_checkdiv_el.appendChild(task_checkbox_el);
         task_el.appendChild(task_checkdiv_el)
 
+        //content
         const task_content_el = document.createElement("div");
         task_content_el.classList.add("content");
         task_content_el.innerText = task;
-
         task_el.appendChild(task_content_el);
 
+        //delete button
+        const delete_div = document.createElement("div");
+        delete_div.classList.add("delete");
+
+        const delete_button = document.createElement("button");
+        delete_button.classList.add("delete-button")
+        delete_button.innerHTML = "Delete";
+
+        delete_div.appendChild(delete_button);
+        task_el.appendChild(delete_div);
+
         list_el.appendChild(task_el);
+
+        input.value = "";
     }
 }
 
