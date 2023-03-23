abd266c0567b1d9e6da24e8016da1ca8783a81be
Aryn02 <98638410+Aryn02@users.noreply.github.com>
2023-03-23 11:45:51 +0600

    cmt

---
+++ lab7/task1/informatics/a/A.py
@@ -0,0 +1,6 @@
+import math
+
+print("hello")
+a = int(input("Vvedite 1 katet: "))
+b = int(input("Vvedite 2 katet: "))
+print(math.sqrt(a**2 + b**2))
---
+++ lab7/task1/informatics/a/B.py
@@ -0,0 +1,3 @@
+a = int(input("Vvedite chislo: "))
+print("The next number for the number", a, "is", a + 1)
+print("The previous number for the number", a, "is", a - 1)
